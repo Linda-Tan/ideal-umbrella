5e5f3677dea48a3b37b7b7f681af33bd706a1413
junliang.com <li926893@163.com>
2018-06-25 11:38:19 +0800

    使用构建模板

---
+++ ideal-consumer/src/main/java/com/github/ideal/consumer/Application.java
@@ -0,0 +1,22 @@
+package com.github.ideal.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * SpringBoot 启动类
+ *
+ * @author junliang
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ ideal-consumer/src/test/java/com/github/ideal/consumer/test/SpringBaseTest.java
@@ -0,0 +1,22 @@
+package com.github.ideal.consumer.test;
+
+import com.github.ideal.consumer.Application;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * SpringBaseTest
+ *
+ * @author junliang
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.DEFINED_PORT)
+public abstract class SpringBaseTest {
+
+    protected static final Logger log = LoggerFactory.getLogger(SpringBaseTest.class);
+
+}
